c443bc1d5788270db65c25a0d8449bfc59e9fa72
Gautam Naik <gautamnaik1994@gmail.com>
2024-09-04 00:12:37 +0530

    updated streamlit app

--- app/main.py
+++ app/main.py
@@ -1,132 +1,172 @@
 import streamlit as st
-from pyspark.sql import SparkSession
-from pyspark.ml import PipelineModel
-from synapse.ml.lightgbm import LightGBMClassifier, LightGBMClassificationModel
-import tarfile
-import os
+import joblib
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+import shap
 
-# Initialize Spark session with the configuration parameter
-spark = SparkSession.builder.appName("Ola")\
-    .config("spark.sql.debug.maxToStringFields", "1000") \
-    .config("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:1.0.5")   \
-    .getOrCreate()
-
-# Load the pre-trained pipeline model
-final_pipeline_model = PipelineModel.load("./app/final_pipeline_model_lgbm")
-
-# Define the input fields and their default values
+st.set_page_config(page_title="OLA Driver Churn Prediction")
 
 
-def initialize_state():
-    return {
-        'Total_Business_Value': 151600,
-        'Total_Had_Negative_Business': 1,
-        'Has_Income_Increased': 0,
-        'Has_Rating_Increased': 0,
-        'Avg_Business_Value': 30320,
-        'Age': 34,
-        'Gender': 0,
-        'Income': 20922,
-        'Total_Income': 104610,
-        'Education_Level': 1,
-        'City': "C29",
-        'Joining_Designation': 2,
-        'Grade': 2,
-        'Quarterly_Rating': 1,
-        'Tenure': 398,
-        'Date_Of_Joining_month': 2,
-        'Date_Of_Joining_year': 2019,
-        'Is_Valuable_Driver': 1
-    }
+@st.cache_resource()
+def load_model():
+    final_pipeline_model = joblib.load(
+        './models/final_pipeline_model_sklearn.pkl')
+    explainer = joblib.load('./models/final_explainer.pkl')
+    return final_pipeline_model, explainer
 
+
+final_pipeline_model, explainer = load_model()
+
+driver_data_intermediate = {
+    'Total_Business_Value': 151600,
+    'Total_Had_Negative_Business': 1,
+    'Has_Income_Increased': 0,
+    'Has_Rating_Increased': 0,
+    'Avg_Business_Value': 30320,
+    'Age': 27,
+    'Gender': 0,
+    'Income': 20922,
+    'Total_Income': 104610,
+    'Education_Level': 1,
+    'City': 'C21',
+    'Joining_Designation': 3,
+    'Grade': 1,
+    'Quarterly_Rating': 2,
+    'Tenure': 1900,
+    'Date_Of_Joining_month': 2,
+    'Date_Of_Joining_year': 2019,
+    'Is_Valuable_Driver': 1
+}
+
+driver_data_default = {
+    'Total_Business_Value': 151600,
+    'Total_Had_Negative_Business': 1,
+    'Has_Income_Increased': 0,
+    'Has_Rating_Increased': 0,
+    'Avg_Business_Value': 30320,
+    'Age': 34,
+    'Gender': 0,
+    'Income': 20922,
+    'Total_Income': 104610,
+    'Education_Level': 1,
+    'City': "C29",
+    'Joining_Designation': 2,
+    'Grade': 2,
+    'Quarterly_Rating': 1,
+    'Tenure': 398,
+    'Date_Of_Joining_month': 2,
+    'Date_Of_Joining_year': 2019,
+    'Is_Valuable_Driver': 1
+}
 
 # Initialize state
-state = initialize_state()
-
-# Define the function to process the input data
+state = driver_data_default
+cities_list = [f"C{i}" for i in range(1, 30)]
 
 
 def process_data(state):
-    input_values = {
-        'Total_Business_Value': state['Total_Business_Value'],
-        'Total_Had_Negative_Business': state['Total_Had_Negative_Business'],
-        'Has_Income_Increased': state['Has_Income_Increased'],
-        'Has_Rating_Increased': state['Has_Rating_Increased'],
-        'Avg_Business_Value': state['Avg_Business_Value'],
-        'Age': state['Age'],
-        'Gender': state['Gender'],
-        'Income': state['Income'],
-        'Total_Income': state['Total_Income'],
-        'Education_Level': state['Education_Level'],
-        'City': state['City'],
-        'Joining_Designation': state['Joining_Designation'],
-        'Grade': state['Grade'],
-        'Quarterly_Rating': state['Quarterly_Rating'],
-        'Tenure': state['Tenure'],
-        'Date_Of_Joining_month': state['Date_Of_Joining_month'],
-        'Date_Of_Joining_year': state['Date_Of_Joining_year'],
-        'Is_Valuable_Driver': state['Is_Valuable_Driver']
-    }
+    print(state)
+    test_feature_df = pd.DataFrame([state])
+    prediction = final_pipeline_model.predict_proba(test_feature_df).round(6)
+    preprocessor = final_pipeline_model.named_steps['preprocessor']
+    test_feature_df = preprocessor.transform(test_feature_df)
+    test_feature_df = pd.DataFrame(
+        test_feature_df, columns=preprocessor.get_feature_names_out())
+    shap_values = explainer(test_feature_df)
 
-    test_feature_df = spark.createDataFrame([input_values])
-    transformed_df = final_pipeline_model.transform(test_feature_df)
-
-    pr = transformed_df.select("prediction", "probability").collect()[0]
-    result = f"{pr['prediction']:.0f}"
-
-    return result
+    return prediction[0], shap_values[0]
 
 
 # Streamlit app layout
 st.title("OLA Driver Churn Prediction")
 
-cols = st.columns(3)
+st.image("./images/ola-driver-churn.png", use_column_width=True)
 
-with cols[0]:
-    state['Total_Business_Value'] = st.number_input(
-        "Total Business Value", value=state['Total_Business_Value'])
-    state['Total_Had_Negative_Business'] = st.number_input(
-        "Total Had Negative Business", value=state['Total_Had_Negative_Business'])
-    state['Has_Income_Increased'] = st.number_input(
-        "Has Income Increased", value=state['Has_Income_Increased'])
+st.subheader("About", divider=True)
 
-with cols[1]:
-    state['Has_Rating_Increased'] = st.number_input(
-        "Has Rating Increased", value=state['Has_Rating_Increased'])
-    state['Avg_Business_Value'] = st.number_input(
-        "Avg Business Value", value=state['Avg_Business_Value'])
-    state['Age'] = st.number_input("Age", value=state['Age'])
+st.markdown(
+    """
+    Following model predicts the probability of a driver churning based on the given details. This model uses LightGBM for prediction. More information can be found on the blog
+    """)
+st.text("")
 
-with cols[2]:
-    state['Gender'] = st.number_input("Gender", value=state['Gender'])
-    state['Income'] = st.number_input("Income", value=state['Income'])
-    state['Total_Income'] = st.number_input(
-        "Total Income", value=state['Total_Income'])
+st.subheader("Personal Details", divider=True)
 
-cols = st.columns(3)
+left, middle, right = st.columns(3, vertical_alignment="center")
 
-with cols[0]:
-    state['Education_Level'] = st.number_input(
-        "Education Level", value=state['Education_Level'])
-    state['City'] = st.text_input("City", value=state['City'])
-    state['Joining_Designation'] = st.number_input(
-        "Joining Designation", value=state['Joining_Designation'])
+state['Age'] = left.number_input("Age", value=state['Age'])
+gender = right.radio("Gender", options=[
+                     "Male", "Female"], index=state['Gender'], horizontal=True)
+state['City'] = middle.selectbox(
+    "City", cities_list, index=20, placeholder="Select a city")
 
-with cols[1]:
-    state['Grade'] = st.number_input("Grade", value=state['Grade'])
-    state['Quarterly_Rating'] = st.number_input(
-        "Quarterly Rating", value=state['Quarterly_Rating'])
-    state['Tenure'] = st.number_input("Tenure", value=state['Tenure'])
+st.markdown("#####")
 
-with cols[2]:
-    state['Date_Of_Joining_month'] = st.number_input(
-        "Date Of Joining Month", value=state['Date_Of_Joining_month'])
-    state['Date_Of_Joining_year'] = st.number_input(
-        "Date Of Joining Year", value=state['Date_Of_Joining_year'])
-    state['Is_Valuable_Driver'] = st.number_input(
-        "Is Valuable Driver", value=state['Is_Valuable_Driver'])
+# Business Details
+st.subheader("Business Details", divider=True)
 
+left, middle, right = st.columns(3)
 
-if st.button("Process Data"):
-    result = process_data(state)
-    st.write(f"Prediction Result: {result}")
+state['Total_Business_Value'] = left.number_input(
+    "Total Business Value", value=state['Total_Business_Value'])
+state['Avg_Business_Value'] = middle.number_input(
+    "Avg Business Value", value=state['Avg_Business_Value'])
+state['Total_Had_Negative_Business'] = right.number_input(
+    "Total Had Negative Business", value=state['Total_Had_Negative_Business'], help="Total number of months the driver had negative business"
+)
+state['Total_Income'] = middle.number_input(
+    "Total Income", value=state['Total_Income'])
+state['Income'] = left.number_input("Income", value=state['Income'])
+state['Has_Income_Increased'] = left.radio(
+    "Has Income Increased", options=[0, 1], index=state['Has_Income_Increased'], horizontal=True
+)
+state['Is_Valuable_Driver'] = middle.radio(
+    "Is Valuable Driver", options=[0, 1], index=state['Is_Valuable_Driver'], horizontal=True, help="Has the driver made more business than total income"
+)
+
+st.markdown("#####")
+
+# Employee Details
+st.subheader("Employee Details", divider=True)
+left, right = st.columns(2)
+
+state['Tenure'] = right.number_input("Tenure", value=state['Tenure'])
+date_of_joining = left.date_input(
+    "Date Of Joining", value=datetime(state['Date_Of_Joining_year'], state['Date_Of_Joining_month'], 1)
+)
+state['Grade'] = left.radio(
+    "Grade", options=[1, 2, 3, 4, 5], index=state['Grade'], horizontal=True)
+state['Joining_Designation'] = right.radio("Joining Designation", options=[
+                                           1, 2, 3, 4, 5], index=state['Joining_Designation'], horizontal=True)
+
+left, middle, right = st.columns([2, 1, 1])
+
+state['Quarterly_Rating'] = left.radio("Quarterly Rating", options=[
+                                       1, 2, 3, 4, 5], index=state['Quarterly_Rating'], horizontal=True)
+state['Education_Level'] = right.radio("Education Level", options=[
+                                       0, 1, 2], index=state['Education_Level'], horizontal=True)
+state['Has_Rating_Increased'] = middle.radio("Has Rating Increased", options=[
+                                             0, 1], index=state['Has_Rating_Increased'], horizontal=True)
+
+state['Date_Of_Joining_month'] = date_of_joining.month
+state['Date_Of_Joining_year'] = date_of_joining.year
+state['Gender'] = 0 if gender == "Male" else 1
+
+st.markdown("#####")
+if st.button("Predict", use_container_width=True, type="primary"):
+    result, shap_values = process_data(state)
+    st.markdown("#####")
+    st.subheader("Prediction Result", divider=True)
+
+    churn_probability = result[1]*100
+    no_churn_probability = result[0]*100
+
+    st.markdown(
+        f"**The probability of the driver churning is :red[{churn_probability}%] and :green[{no_churn_probability}%] of not churning**", unsafe_allow_html=True)
+
+    st.markdown("#### Model Interpretation")
+    fig, ax = plt.subplots()
+    shap.waterfall_plot(shap_values, max_display=10, show=False)
+    st.pyplot(fig)
+    st.text("The above plot shows how individual features contribute to the prediction.")
---
+++ streamlit_pyspark/main.py
@@ -0,0 +1,132 @@
+import streamlit as st
+from pyspark.sql import SparkSession
+from pyspark.ml import PipelineModel
+from synapse.ml.lightgbm import LightGBMClassifier, LightGBMClassificationModel
+import tarfile
+import os
+
+# Initialize Spark session with the configuration parameter
+spark = SparkSession.builder.appName("Ola")\
+    .config("spark.sql.debug.maxToStringFields", "1000") \
+    .config("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:1.0.5")   \
+    .getOrCreate()
+
+# Load the pre-trained pipeline model
+final_pipeline_model = PipelineModel.load("./app/final_pipeline_model_lgbm")
+
+# Define the input fields and their default values
+
+
+def initialize_state():
+    return {
+        'Total_Business_Value': 151600,
+        'Total_Had_Negative_Business': 1,
+        'Has_Income_Increased': 0,
+        'Has_Rating_Increased': 0,
+        'Avg_Business_Value': 30320,
+        'Age': 34,
+        'Gender': 0,
+        'Income': 20922,
+        'Total_Income': 104610,
+        'Education_Level': 1,
+        'City': "C29",
+        'Joining_Designation': 2,
+        'Grade': 2,
+        'Quarterly_Rating': 1,
+        'Tenure': 398,
+        'Date_Of_Joining_month': 2,
+        'Date_Of_Joining_year': 2019,
+        'Is_Valuable_Driver': 1
+    }
+
+
+# Initialize state
+state = initialize_state()
+
+# Define the function to process the input data
+
+
+def process_data(state):
+    input_values = {
+        'Total_Business_Value': state['Total_Business_Value'],
+        'Total_Had_Negative_Business': state['Total_Had_Negative_Business'],
+        'Has_Income_Increased': state['Has_Income_Increased'],
+        'Has_Rating_Increased': state['Has_Rating_Increased'],
+        'Avg_Business_Value': state['Avg_Business_Value'],
+        'Age': state['Age'],
+        'Gender': state['Gender'],
+        'Income': state['Income'],
+        'Total_Income': state['Total_Income'],
+        'Education_Level': state['Education_Level'],
+        'City': state['City'],
+        'Joining_Designation': state['Joining_Designation'],
+        'Grade': state['Grade'],
+        'Quarterly_Rating': state['Quarterly_Rating'],
+        'Tenure': state['Tenure'],
+        'Date_Of_Joining_month': state['Date_Of_Joining_month'],
+        'Date_Of_Joining_year': state['Date_Of_Joining_year'],
+        'Is_Valuable_Driver': state['Is_Valuable_Driver']
+    }
+
+    test_feature_df = spark.createDataFrame([input_values])
+    transformed_df = final_pipeline_model.transform(test_feature_df)
+
+    pr = transformed_df.select("prediction", "probability").collect()[0]
+    result = f"{pr['prediction']:.0f}"
+
+    return result
+
+
+# Streamlit app layout
+st.title("OLA Driver Churn Prediction")
+
+cols = st.columns(3)
+
+with cols[0]:
+    state['Total_Business_Value'] = st.number_input(
+        "Total Business Value", value=state['Total_Business_Value'])
+    state['Total_Had_Negative_Business'] = st.number_input(
+        "Total Had Negative Business", value=state['Total_Had_Negative_Business'])
+    state['Has_Income_Increased'] = st.number_input(
+        "Has Income Increased", value=state['Has_Income_Increased'])
+
+with cols[1]:
+    state['Has_Rating_Increased'] = st.number_input(
+        "Has Rating Increased", value=state['Has_Rating_Increased'])
+    state['Avg_Business_Value'] = st.number_input(
+        "Avg Business Value", value=state['Avg_Business_Value'])
+    state['Age'] = st.number_input("Age", value=state['Age'])
+
+with cols[2]:
+    state['Gender'] = st.number_input("Gender", value=state['Gender'])
+    state['Income'] = st.number_input("Income", value=state['Income'])
+    state['Total_Income'] = st.number_input(
+        "Total Income", value=state['Total_Income'])
+
+cols = st.columns(3)
+
+with cols[0]:
+    state['Education_Level'] = st.number_input(
+        "Education Level", value=state['Education_Level'])
+    state['City'] = st.text_input("City", value=state['City'])
+    state['Joining_Designation'] = st.number_input(
+        "Joining Designation", value=state['Joining_Designation'])
+
+with cols[1]:
+    state['Grade'] = st.number_input("Grade", value=state['Grade'])
+    state['Quarterly_Rating'] = st.number_input(
+        "Quarterly Rating", value=state['Quarterly_Rating'])
+    state['Tenure'] = st.number_input("Tenure", value=state['Tenure'])
+
+with cols[2]:
+    state['Date_Of_Joining_month'] = st.number_input(
+        "Date Of Joining Month", value=state['Date_Of_Joining_month'])
+    state['Date_Of_Joining_year'] = st.number_input(
+        "Date Of Joining Year", value=state['Date_Of_Joining_year'])
+    state['Is_Valuable_Driver'] = st.number_input(
+        "Is Valuable Driver", value=state['Is_Valuable_Driver'])
+
+
+if st.button("Process Data"):
+    result = process_data(state)
+    st.write(f"Prediction Result: {result}")
